24fa211b51feae8b8f4eb9abf5f181a70cec2339
Michael Muzzarelli <muzzarellimj@outlook.com>
2023-03-31 13:31:29 -0400

    Create requestMany function and start script

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ const baseUrl = "https://starwars-visualguide.com/assets/img"
 
 // asset name schema matches with SWAPI source URI so resource set count is predictable
 const count = {
-	people: 82,
+	characters: 82,
 	planets: 60,
 	films: 6,
 	species: 37,
 	vehicles: 39,
 	starships: 36
+}
+
+requestMany('characters', count.characters)
+requestMany('planets', count.planets)
+requestMany('films', count.films)
+requestMany('species', count.species)
+requestMany('vehicles', count.vehicles)
+requestMany('starships', count.starships)
+
+function requestMany(type, count) {
+	// count does not consider resources start at id = 1 so increment count in loop
+	for (let i = 1; i <= count + 1; i++) {
+		request(baseUrl, type, i)
+	}
 }
 
 function request(base, type, id) {
